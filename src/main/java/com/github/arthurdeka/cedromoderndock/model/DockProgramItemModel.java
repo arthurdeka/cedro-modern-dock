50ab95792981e5fa8cc02dc307d8743815194fa6
Arthur <arthurdk01@gmail.com>
2025-07-05 11:56:01 -0300

    bugfix/add elevation handling for program execution
    Some programs request elevation to be run. Added handling for this case.

--- src/main/java/com/github/arthurdeka/cedromoderndock/model/DockProgramItemModel.java
+++ src/main/java/com/github/arthurdeka/cedromoderndock/model/DockProgramItemModel.java
@@ -47,16 +47,43 @@ public class DockProgramItemModel implements DockItem {
 
         // tries to open the software
         try {
-            ProcessBuilder pb = new ProcessBuilder(exePath);
-            pb.start();
-            System.out.println("Executing: " + label);
-
+            executeAndHandleElevation(exePath);
         } catch (IOException e) {
             System.err.println("Failed to open: " + label);
             System.err.println("Path: " + exePath);
             System.err.println("Error: " + e.getMessage());
             // Un-comment the line below to debug the stack trace
             // e.printStackTrace();
+        } catch (InterruptedException e) {
+            System.out.println("Process interrupted: " + e.getMessage());
+            throw new RuntimeException(e);
         }
     }
+
+    private void executeAndHandleElevation(String path) throws IOException, InterruptedException {
+        try {
+            // First try: open the software as normal permission level
+            new ProcessBuilder(path).start();
+            System.out.println("Executing: " + label);
+
+        } catch (IOException e) {
+            /*
+            * IF FAILED
+            * error for elevation is "error=740".
+            * In other words, that means the software requires to be run as administrator
+            * So what we'll do is to ask the user permission to run it as administrator
+            * */
+            if (e.getMessage() != null && e.getMessage().contains("error=740")) {
+                // if the code matches, we try again
+                System.out.println("Standard execution failed. Requesting elevation...");
+                String command = "Start-Process -FilePath '" + path + "' -Verb RunAs";
+                new ProcessBuilder("powershell.exe", "-Command", command).start();
+                System.out.println("(Elevated) Executing: " + label);
+            } else {
+                // Error for other reason
+                throw e;
+            }
+        }
+    }
+
 }
